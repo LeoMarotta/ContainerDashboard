6a5b2d8a0c9c5ff453d3cc006972f0eab344999f
LeoMarotta <sen.aka@hotmail.com>
2024-10-24 21:50:47 -0300

    Boot

---
+++ src/main/java/com/quack/containerdashboard/ContainerdashboardApplication.java
@@ -0,0 +1,13 @@
+package com.quack.containerdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerdashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerdashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quack/containerdashboard/ContainerdashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quack.containerdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerdashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
